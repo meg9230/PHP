9b87b55ade95491b57d99773cdf7706adb0046f3
Megumi Kutsuzawa <mgm.ktzw07@gmail.com>
2020-08-21 02:38:27 +0000

    updated

--- practice.php
+++ practice.php
@@ -8,14 +8,14 @@ $array_month = ["1月", "2月", "3月","4月","5月","6月","7月","8月","9月"
 echo $array_month[7];
 echo "\n";
 
-$hello = "Hello";
+$hello = "Hello, ";
 $name = "Megumi";
 $world = "'s World!";
-echo $hello. ', ' . $name. $world;
+echo $hello. $name. $world;
 echo "\n";
 
 $tech_boost = "tech";
-$tech_boost .= "_boost";
+$tech_boost .= " boost";
 echo $tech_boost;
 echo "\n";
 
@@ -33,5 +33,5 @@ $calendar_2018 = [
   "November" => "11月",
   "December" => "12月"
     ];
-    echo $calendar_2018["December"];
+echo $calendar_2018["December"];
     
